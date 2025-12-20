649c8d8c533534f513f3831c504358f8849bc90d
mugeshr19 <mugesh.r1906@gmail.com>
2025-12-20 17:58:30 +0530

    added code that will fetch the questions url from the graphql

---
+++ server/api_function/leetquestion.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const leetquestion = async(req,res)=>{
+    const {number} = req.params;
+    try{
+        const respose = await axios.post("https://leetcode.com/graphql",{
+            query:`
+            query questionData($number: Int!) {
+                questionList(
+                    categorySlug : ""
+                    filter: { frontendQuestionId: $number }
+                ){
+                    questions{
+                        frontendQuestionId
+                        title
+                        titleSlug
+                        difficulty
+                    }
+                }
+            }
+            `,
+            variables:{
+                number : Number(number)
+            }
+        },
+    {
+      headers:{
+        "Content-Type": "application/json"
+      }  
+    });
+        const question = respose.data.data.questionList.questions[0];
+
+        if(!question){
+            return  res.status(404).json({ error: "Question not found" });
+        }
+
+        res.json({
+            problemNumber: question.frontendQuestionId,
+            title: question.title,
+            difficulty: question.difficulty,
+            url: `https://leetcode.com/problems/${question.titleSlug}/`
+        });
+    } catch(error){
+        console.error("Error fetching question:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+export {leetquestion};
--- server/routes/basic_routes.js
+++ server/routes/basic_routes.js
@@ -2,8 +2,10 @@ import express from "express";
 import {
     leetcode,   
 } from "../api_function/leetcode.js";
+import {leetquestion } from "../api_function/leetquestion.js";
 
 const router = express.Router();
 router.get("/profile/:username", leetcode);
+router.get("/question/:number", leetquestion);
 
 export default router;
